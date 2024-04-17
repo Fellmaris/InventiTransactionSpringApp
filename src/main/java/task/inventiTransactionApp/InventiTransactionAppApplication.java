2677d618a7d640ca6bdcd571360b02aa97c5adb3
Fellmaris <cerniauskas.arturas@gmail.com>
2024-04-17 12:24:03 +0300

    Spring boot init commit

---
+++ src/main/java/task/inventiTransactionApp/InventiTransactionAppApplication.java
@@ -0,0 +1,13 @@
+package task.inventiTransactionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventiTransactionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventiTransactionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/task/inventiTransactionApp/InventiTransactionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package task.inventiTransactionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventiTransactionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
